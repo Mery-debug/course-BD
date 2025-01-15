243aa06807b24d2244bb8cdcb5e04a62ce0132b0
Mery-debug <m.a.shap58000@gmail.com>
2025-01-15 05:11:34 +0300

    API give only 10 company, finaly!

--- src/API.py
+++ src/API.py
@@ -6,26 +6,28 @@ from dotenv import load_dotenv
 class APIhh:
     def __init__(self, word):
         self.word: str = word
-        load_dotenv()
-        for name in os.getenv('lst'):
-            self.params = {'text': name, 'page': 0, 'per_page': 100, 'only_with_vacancies': 'only_with_vacancies'}
-        self.headers = {'name': ''}
+        self.params = {}
+        self.headers = {'text': ''}
         self.company = []
 
-    def search_api_hh_clients(self):
-        company = [{}]
-        url = "https://api.hh.ru/employers"
-        while self.params.get('page') != 50:
-            r = requests.get(url, headers=self.headers, params=self.params)
-            if r.status_code == 200:
-                company = r.json()['items']
-                self.company.extend(company)
-                self.params['page'] += 1
-            else:
-                return f'Возможная причина {r.reason}'
-        # return company
-        return f"id {[com.get('id') for com in company]}, \nname {[com.get('name') for com in company]}, " \
-               f"\nopen_vacancies {[com.get('open_vacancies') for com in company]}"
+    def search_api_hh_clients(self, company=None):
+        if company is None:
+            company = [{}]
+        url = "https://api.hh.ru/employers/"
+        load_dotenv()
+        for name in os.getenv('lst'):
+            self.params = {'sort_by': 'by_vacancies_open', 'text': name, 'page': 0, 'per_page': 10,
+                           'only_with_vacancies': 'only_with_vacancies'}
+            while self.params.get('page') != 10:
+                r = requests.get(url, headers=self.headers, params=self.params)
+                if r.status_code == 200:
+                    company = r.json()['items']
+                    self.company.extend(company)
+                    self.params['page'] += 1
+                    return f"id {[com.get('id') for com in company]}, \nname {[com.get('name') for com in company]}, " \
+                           f"\nopen_vacancies {[com.get('open_vacancies') for com in company]}"
+                else:
+                    return f'Возможная причина {r.reason}'
 
 
 p = APIhh('python').search_api_hh_clients()
