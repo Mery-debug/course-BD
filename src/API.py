74ffe99f88d1786b6b96ac0ee8bcb97b069648e8
Mery-debug <m.a.shap58000@gmail.com>
2025-01-16 21:44:21 +0300

    decorator

--- src/API.py
+++ src/API.py
@@ -4,6 +4,7 @@ import requests
 import os
 from dotenv import load_dotenv
 from beautifultable import BeautifulTable
+from src.decoration import wrapper
 
 
 class APIhh:
@@ -14,10 +15,12 @@ class APIhh:
         self.company = []
         self.vacancies = []
 
-    def search_api_hh_clients(self, company: list[dict] = None) -> Any[str | list[dict]]:
+    # @wrapper
+    def search_api_hh_clients(self, company: list[dict] = None) -> Any:
         """Метод для работы с апи и получении информации о работодателях"""
         if company is None:
             company = [{}]
+        compan = {}
         url = "https://api.hh.ru/employers/"
         load_dotenv()
         for name in os.getenv('lst'):
@@ -26,13 +29,21 @@ class APIhh:
             while self.params.get('page') != 10:
                 r = requests.get(url, headers=self.headers, params=self.params)
                 if r.status_code == 200:
-                    company = r.json()['items']
+                    for com in r.json()['items']:
+                        compan = {
+                            'id': int(com["id"]),
+                            'name': com["name"],
+                            'url': com["url"],
+                            'open_vacancies': com["open_vacancies"]
+                        }
+                        company.append(compan)
                     self.company.extend(company)
                     self.params['page'] += 1
                     return company
                 else:
                     return f'Возможная причина {r.reason}'
 
+    # @wrapper
     def search_api_vacancies(self, company: list[dict]) -> list[dict]:
         """
         Поиск вакансий для найденных компаний
@@ -40,8 +51,8 @@ class APIhh:
         vacancie = []
         vacan = {}
         url = "https://api.hh.ru/vacancies"
-        com = [com['id'] for com in company]
-        self.params = {'text': self.word, 'employer_id': com, 'page': 0, 'per_page': 100}
+        co = [com.get('id') for com in company]
+        self.params = {'text': self.word, 'employer_id': co, 'page': 0, 'per_page': 100}
         while self.params.get('page') != 20:
             r = requests.get(url, params=self.params)
             if r.status_code == 200:
@@ -60,27 +71,29 @@ class APIhh:
                         vacancie.append(vacan)
             return vacancie
 
-
-def expectation_table_employees(compani: list[dict]) -> BeautifulTable:
-    """ Функция для красивого отображения результатов поиска в таблице о работодателях"""
-    table = BeautifulTable()
-    table.column_headers = ["id", "company", "url", "open vacancies"]
-    for com in compani:
-        table.append_row([com['id'], com['name'], com['url'], com['open_vacancies']])
-    return table
-
-
-def expectation_table_vacancies(vacancie: list[dict]) -> BeautifulTable:
-    """Функция отображения результатов работы апи вакансий найденных в первом апи-методе компаний"""
-    table = BeautifulTable()
-    table.column_headers = ["vacancie id", "name", "salary from", "salary to", "url", "employer id"]
-    for vac in vacancie:
-        table.append_row([vac['vacancie_id'], vac['name'], vac['salary']['from'], vac['salary']['to'], vac['url'],
-                          vac['employer_id']])
-    return table
-
-
+#
+# def expectation_table_employees(compani: list[dict]) -> BeautifulTable:
+#     """ Функция для красивого отображения результатов поиска в таблице о работодателях"""
+#     table = BeautifulTable()
+#     table.column_headers = ["id", "company", "url", "open vacancies"]
+#     for com in compani:
+#         table.append_row([com['id'], com['name'], com['url'], com['open_vacancies']])
+#     return table
+#
+#
+# def expectation_table_vacancies(vacancie: list[dict]) -> BeautifulTable:
+#     """Функция отображения результатов работы апи вакансий найденных в первом апи-методе компаний"""
+#     table = BeautifulTable()
+#     table.column_headers = ["vacancie id", "name", "salary from", "salary to", "url", "employer id"]
+#     for vac in vacancie:
+#         table.append_row([vac['vacancie_id'], vac['name'], vac['salary']['from'], vac['salary']['to'], vac['url'],
+#                           vac['employer_id']])
+#     return table
+#
+#
 # p = APIhh('python').search_api_hh_clients()
+# print(p)
 # print(expectation_table_employees(compani=p))
 # v = APIhh('python').search_api_vacancies(company=p)
+# print(v)
 # print(expectation_table_vacancies(vacancie=v))
---
+++ src/decoration.py
@@ -0,0 +1,19 @@
+from typing import Any, Callable
+from functools import wraps
+from beautifultable import BeautifulTable
+
+
+def wrapper(func: Callable) -> Callable:
+    """Декоратор для выведения результатов в таблицу"""
+
+    @wraps(func)
+    def inner(*args: Any, **kwargs: Any) -> Any:
+        comp = func(*args, **kwargs)
+        table = BeautifulTable()
+        table.column_headers = comp[0].keys()
+        for dic in comp:
+            table.append_row(list(dic.values()))
+
+        return table
+
+    return inner
